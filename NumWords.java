c31a7fe16255ea5b2e5a18d47e06ab4365c8b099
asafaloush <asafaloush@gmail.com>
2024-11-07 16:42:10 +0200

    Update NumWords.java

--- NumWords.java
+++ NumWords.java
@@ -5,6 +5,6 @@ public class NumWords {
 		int ones=(n%10);
 		int tens= (n/10)%10;
 		int hundreds = (n/100)%10;
-		System.out.println(hundreds + " hundreds, " + tens + " tens, " + ones + " ones.");
+		System.out.println(hundreds + " hundreds, " + tens + " tens, and " + ones + " ones.");
 	}
 }
